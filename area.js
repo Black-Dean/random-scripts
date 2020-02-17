0a7f881a5b626887363d701ef28ee97b8dea058c
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-02-17 11:42:24 +0000

    Creating area.js

---
+++ area.js
@@ -0,0 +1,9 @@
+var l; var a; var w;
+l = parseInt(prompt("Enter length: "));
+w = parseInt(prompt("Enter width: "));
+a = l * w;
+if (l == w) {
+  alert("The Area of the square is " + a);
+} else {
+  alert ("The area of the rectangle is " + a);
+}
