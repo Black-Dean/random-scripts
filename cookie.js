a91a2cde7a74d1f5f844657085ddeb4cc36ad06a
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-02-17 12:41:17 +0000

    Check for number & rounding
    Checks if the user entered a number if not then it is set to 0, also added rounding fo singe and batch profit

--- cookie.js
+++ cookie.js
@@ -5,8 +5,18 @@ var cookieCost = (batchCost / 50); // Get cost per cookie and round to two decim
 cookieCost = cookieCost.toFixed(2);
 
 cookiePrice = Number(prompt("How much are you selling each cookie for? ")); // Get price of each cookie
+console.log(cookiePrice)
+if (isNaN(cookiePrice)) { // Checks for number
+  alert("You have not entered a number 0 has been set as default");
+  cookiePrice = 0;
+}
 
-singleProfit = cookiePrice - cookieCost; // Calculate profits
+singleProfit = cookiePrice - cookieCost; // Calculate profits and rounds
+singleProfit = singleProfit.toFixed(2);
 batchProfit = singleProfit * 50;
+batchProfit = batchProfit.toFixed(2);
 
-alert("It will cost " + batchCost + " to make 50 cookies." + "\nTo make one cookie it will cost " + cookieCost + "\nYour profit per cookie is £" + singleProfit + " per cookie." + "\nMeaning your profit per batch is £" + batchProfit + "."); // Outputs data
+
+alert("It will cost " + batchCost + " to make 50 cookies." + "\nTo make one cookie it will cost " + cookieCost + "\nYour profit per cookie is £" + singleProfit + " per cookie." + "\nMeaning your profit per batch is £" + batchProfit + ".");
+
+
