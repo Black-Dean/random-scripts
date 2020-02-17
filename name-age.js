42d796af8fd146e0c20b2913b4f9ce585698e95f
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-02-17 12:07:52 +0000

    Creating name-age.js

---
+++ name-age.js
@@ -0,0 +1,4 @@
+var name = String(prompt("What is your name? ")); // Asks for name
+var age = String(prompt("How old are you? ")); // Asks for age
+
+alert("Your name is " + name + " and you are "+ age+ " years old." ); // Outputs data in sentence
