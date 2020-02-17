1b05b71715787875d0ab85e3047479258da02bfe
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-02-17 11:47:35 +0000

    Adding comments

--- area.js
+++ area.js
@@ -1,8 +1,8 @@
-var l; var a; var w;
-l = parseInt(prompt("Enter length: "));
-w = parseInt(prompt("Enter width: "));
-a = l * w;
-if (l == w) {
+var l; var a; var w; // Declare variables
+l = parseInt(prompt ("Enter length: ")); // Ask for length
+w = parseInt(prompt ("Enter width: ")); // Ask for width
+a = l * w; // Multiple length by width
+if (l === w) { // Check if its a square or rectangle and use appropriate output.
   alert("The Area of the square is " + a);
 } else {
   alert ("The area of the rectangle is " + a);
