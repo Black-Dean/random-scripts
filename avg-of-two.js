6a1323503faedf11cbb69a3380143289b7389af8
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-02-17 11:50:38 +0000

    Adding comments

--- avg-of-two.js
+++ avg-of-two.js
@@ -1,4 +1,4 @@
-var num1 = parseInt(prompt("Enter a first number :"));
-var num2 = parseInt (prompt("Enter a 2nd number : "));
-var avg = (num1 + num2) / 2;
-alert ("The adverage was: " + avg)
+var num1 = parseInt(prompt("Enter a first number :")); // Ask for first number
+var num2 = parseInt (prompt("Enter a second number : ")); // Ask for second number
+var avg = (num1 + num2) / 2; // Add them together and divide them
+alert ("The adverage was: " + avg) // Output the adverage
