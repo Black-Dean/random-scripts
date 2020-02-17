6a230961365ee9e94280e661758279e3c9c04310
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-02-17 11:53:22 +0000

    Adding comments

--- guess.js
+++ guess.js
@@ -1,21 +1,21 @@
-var min = 1, max = 20, guess, correct = prompt("Enter a number:");
+var min = 1, max = 20, guess, correct = prompt("Enter a number:"); // Declare variables
 
-function getGuess(min, max) {
-    guess = Math.floor((max + min) / 2);
-    isCorrect();
+function getGuess(min, max) { // Create function using variables
+    guess = Math.floor((max + min) / 2); // Get rid of decimal and divide by two
+    isCorrect(); // Call isCorrect function
 }
-function isCorrect() {
-    if (guess != correct) {
-        if (guess < correct) {
+function isCorrect() { // Declare function
+    if (guess != correct) { // If guess is not correct min continue
+        if (guess < correct) { // If guess less than correct min = guess
             min = guess;
         }
-        if (guess > correct) {
+        if (guess > correct) { // If guess more than correct max = guess
             max = guess;
         }
-        getGuess(min, max);
-    } else if (guess == correct) {
+        getGuess(min, max); // Call getGuess function
+    } else if (guess == correct) { // Else if guess is correct out put the number is guess and end script
         alert("The number was " + guess);
         return process.exit(1);
     }
 }
-getGuess(min, max);
+getGuess(min, max); // Call getGuess function
