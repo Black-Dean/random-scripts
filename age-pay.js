8dc8e19b5cbd7a101890fe5df898c07fd4a52e95
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-02-24 11:24:55 +0000

    Create age-pay.js

---
+++ age-pay.js
@@ -0,0 +1,9 @@
+age = parseInt(prompt("How old are you? ")); // Asks for age
+if (isNaN(age)) { // Checks if input is not a number
+	alert("You did not enter a number.") // Tells them its not a number
+}
+if (age > 4) { // Is age above 4
+	alert("You must pay £3") // If it is they need to pay £3
+} else if (age <= 4) { // If the age is less than or equal to 4 then they do not need to pay
+	alert("You do not have to pay.")
+}
