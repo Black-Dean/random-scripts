fce5a54b53dfda815b4f2d94b6a6529aeb5718c0
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-02-24 12:15:03 +0000

    Update like-js.js

--- like-js.js
+++ like-js.js
@@ -1,4 +1,4 @@
-var yes = "y"; var no = "n"; var answer = String(prompt("Do you like javascript? ")); // Sets all variables and aks them for input
+var yes = "y", no = "n", answer = String(prompt("Do you like javascript? ")); // Sets all variables and aks them for input
 var firstChar = answer.charAt(0) // Takes first character of their input so if they use the words Yes or No it will still work
 if (firstChar.toUpperCase() === yes.toUpperCase()) { // This makes it case insensitive & checks if it is equal to yes
 	alert("You have good taste!");
