cac3de2c49d03b22ca2a4ff97ea9e54cf98d2c71
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-02-24 12:14:01 +0000

    Update like-js.js

--- like-js.js
+++ like-js.js
@@ -1,5 +1,5 @@
-yes = "y"; no = "n"; answer = String(prompt("Do you like javascript? ")); // Sets all variables and aks them for input
-firstChar = answer.charAt(0) // Takes first character of their input so if they use the words Yes or No it will still work
+var yes = "y"; var no = "n"; var answer = String(prompt("Do you like javascript? ")); // Sets all variables and aks them for input
+var firstChar = answer.charAt(0) // Takes first character of their input so if they use the words Yes or No it will still work
 if (firstChar.toUpperCase() === yes.toUpperCase()) { // This makes it case insensitive & checks if it is equal to yes
 	alert("You have good taste!");
 } else if (firstChar.toUpperCase() === no.toUpperCase()) {
