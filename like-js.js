a29c911ff547d14c4846c2460d4447f4d6b48996
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-02-24 11:42:51 +0000

    Create like-js.js

---
+++ like-js.js
@@ -0,0 +1,9 @@
+yes = "y"; no = "n"; answer = String(prompt("Do you like javascript? ")); // Sets all variables and aks them for input
+firstChar = answer.charAt(0) // Takes first character of their input so if they use the words Yes or No it will still work
+if (firstChar.toUpperCase() === yes.toUpperCase()) { // This makes it case insensitive & checks if it is equal to yes
+	alert("You have good taste!");
+} else if (firstChar.toUpperCase() === no.toUpperCase()) {
+	alert("We can’t all have brains!")
+} else {
+	alert("You did not answer correctly.") // If they input anything that doesn't start with Y or N
+}
