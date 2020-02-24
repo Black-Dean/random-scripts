b1f776d2cd9580362840057f1c791846c18ce309
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-02-24 12:12:36 +0000

    Update area.js

--- area.js
+++ area.js
@@ -1,9 +1,9 @@
-var l; var a; var w; // Declare variables
+var l, a, w; // Declare variables
 l = parseInt(prompt ("Enter length: ")); // Ask for length
 w = parseInt(prompt ("Enter width: ")); // Ask for width
 a = l * w; // Multiple length by width
 if (l === w) { // Check if its a square or rectangle and use appropriate output.
-  alert("The Area of the square is " + a);
+  alert("The Area of the square is " + a + "in area.");
 } else {
-  alert ("The area of the rectangle is " + a);
+  alert ("The area of the rectangle is " + a + "in area.");
 }
