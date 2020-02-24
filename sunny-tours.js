03c3e0eb577aee3294d67bf55721172b97f0057c
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-02-24 12:07:08 +0000

    Adding comments

--- sunny-tours.js
+++ sunny-tours.js
@@ -1,7 +1,7 @@
-var total; var adults = parseInt(prompt("How many adults are there?")); var children = parseInt(prompt("How many children are there?"));
-if (isNaN(adults) || isNaN(children)) {
+var total; var adults = parseInt(prompt("How many adults are there?")); var children = parseInt(prompt("How many children are there?")); // Declare all vars and asks for amount of children and adults
+if (isNaN(adults) || isNaN(children)) { // Checks if the input was a number
 	alert("Sorry but it seems as if you haven't entered your numbers correctly.")
 }else { 
-	total = (adults * 60) + (adults * 210) + (children * 20) + (children * 110) 
-	alert("There are " + adults + " adults & " + children + " children.\nMaking the total cost £" + total + ".")
+	total = (adults * 60) + (adults * 210) + (children * 20) + (children * 110) // Gets the total
+	alert("There are " + adults + " adults & " + children + " children.\nMaking the total cost £" + total + ".") // Gives an output
 }
