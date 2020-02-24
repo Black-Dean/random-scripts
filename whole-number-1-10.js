4fe2e9ca6eccf0ed4144ec3aacef4a2dc6138d0d
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-02-24 12:15:26 +0000

    Update whole-number-1-10.js

--- whole-number-1-10.js
+++ whole-number-1-10.js
@@ -1,4 +1,4 @@
-num = parseInt(prompt("Please enter a number between 1 and 10")) // Parse int will make it so the user cannot enter decimal numbers and will take everything they entered up to the decimal, this ensures that they have entered a full number rather than a decimal number.
+var num = parseInt(prompt("Please enter a number between 1 and 10")) // Parse int will make it so the user cannot enter decimal numbers and will take everything they entered up to the decimal, this ensures that they have entered a full number rather than a decimal number.
 if (isNaN(num)) {
   alert("You didn't enter a number") // This will make sure they've entered a number
 } else if (num >= 1 && num <= 10) { // This checks if the number is in the range
