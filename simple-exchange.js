3e8d296c54cc0c5cb6a8c7c743cc48110c9c3acc
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-03-02 12:37:47 +0000

    Adds rounding

--- simple-exchange.js
+++ simple-exchange.js
@@ -3,11 +3,13 @@ var pounds, euros, exchange = 1.19; // Declares variables
 pounds = parseInt(prompt("How many pounds do you want to exchange?")) // Asks them how much pounds they want to exchange
 if (pounds > 0) { // Makes sure amount is more than 0
 	euros = pounds * exchange; // Calculates euros
+  euros = Math.round(euros * 100) / 100 // rounds
   if (pounds > 500) { // If its more than 500 it'll go here to avoid comission
   	alert("In exchange for £" + pounds + " you will receive " + euros + " euros."); // Tells them how much they get
     }
   else if (pounds <= 500) { // If its equal to or less than 500 it comes here
   	euros = euros - ((euros / 100)*3) // Adds commission
+    euros = Math.round(euros * 100) / 100 // Due to new commission this is required
     alert("In exchange for £" + pounds + " you will receive " + euros + " euros."); // Tells them how much they get
   }
   } else if (pounds <= 0) {
