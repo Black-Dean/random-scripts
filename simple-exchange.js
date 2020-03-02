58f22fa5b53aa41bddcaab8d39e8a2547212598a
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-03-02 12:39:55 +0000

    Fits brief more

--- simple-exchange.js
+++ simple-exchange.js
@@ -1,6 +1,6 @@
 var pounds, euros, exchange = 1.19; // Declares variables
 
-pounds = parseInt(prompt("How many pounds do you want to exchange?")) // Asks them how much pounds they want to exchange
+pounds = prompt("How many pounds do you want to exchange?") // Asks them how much pounds they want to exchange
 if (pounds > 0) { // Makes sure amount is more than 0
 	euros = pounds * exchange; // Calculates euros
   euros = Math.round(euros * 100) / 100 // rounds
@@ -12,6 +12,6 @@ if (pounds > 0) { // Makes sure amount is more than 0
     euros = Math.round(euros * 100) / 100 // Due to new commission this is required
     alert("In exchange for £" + pounds + " you will receive " + euros + " euros."); // Tells them how much they get
   }
-  } else if (pounds <= 0) {
-  	alert("Input error please input a number greater than 0.") // If someone inputs less than or equal to 0
+  } else if (pounds <= 0 || pounds == " ") {
+  	alert("Input error please input a number greater than 0.") // If someone inputs less than or equal to 0 or space
   } else {alert("Invalid Entry.")} // If someone uses a letter
