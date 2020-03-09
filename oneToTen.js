53074d73501a9e37c2c98515a6c1e893e280b0ad
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-03-09 11:30:37 +0000

    Update and rename whole-number-1-10.js to oneToTen.js

--- oneToTen.js
+++ oneToTen.js
@@ -1,3 +1,5 @@
+// Dean Black - oneToTen.js
+
 var num = parseInt(prompt("Please enter a number between 1 and 10")) // Parse int will make it so the user cannot enter decimal numbers and will take everything they entered up to the decimal, this ensures that they have entered a full number rather than a decimal number.
 if (isNaN(num)) {
   alert("You didn't enter a number") // This will make sure they've entered a number
