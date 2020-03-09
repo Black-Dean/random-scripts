ac09a360b088928ce6ac7ca29a348034690e712e
Black-Dean <39032636+Black-Dean@users.noreply.github.com>
2020-03-09 11:25:02 +0000

    Update and rename avg-of-two.js to avgOfTwo.js

--- avgOfTwo.js
+++ avgOfTwo.js
@@ -1,3 +1,4 @@
+// Dean Black - avgOfTwo.js
 var num1 = parseInt(prompt("Enter a first number :")); // Ask for first number
 var num2 = parseInt (prompt("Enter a second number : ")); // Ask for second number
 var avg = (num1 + num2) / 2; // Add them together and divide them
